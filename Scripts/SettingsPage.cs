0d25f951bc3ac3529c683d8330e0253444ec5565
Akarsh Chauhan <akarshchauhan15@gmail.com>
2025-08-09 17:49:17 +0530

    Updated

--- Scripts/SettingsPage.cs
+++ Scripts/SettingsPage.cs
@@ -40,10 +40,17 @@ public partial class SettingsPage : Control
             {
                 IsDragging = false;
 <<<<<<< HEAD
+<<<<<<< Updated upstream
                 float Delta = (Touch.Position.X - StartPosition.X); //* (float) (1 + 1/(DragTime*100));
 =======
                 float Delta = (Touch.Position.X - StartPosition.X) * (float) (1 + 1/(DragTime*100));
 >>>>>>> main
+=======
+                float Delta = (Touch.Position.X - StartPosition.X) * (float) (1 + 1/(DragTime*100));
+=======
+                float Delta = (Touch.Position.X - StartPosition.X); //* (float) (1 + 1/(DragTime*100));
+>>>>>>> dev
+>>>>>>> Stashed changes
                 Delta /= 300;
 
                 if (Delta < -1) CurrentPage++;
@@ -66,6 +73,7 @@ public partial class SettingsPage : Control
         Tween tween = CreateTween();
         tween.SetParallel(true);
 <<<<<<< HEAD
+<<<<<<< Updated upstream
         tween.TweenCallback(Callable.From(() =>  LabelContainer.MouseFilter = MouseFilterEnum.Ignore));
         tween.TweenCallback(Callable.From(() =>  MouseFilter = MouseFilterEnum.Ignore));
 
@@ -80,6 +88,22 @@ public partial class SettingsPage : Control
         tween.TweenProperty(LabelContainer, "scroll_horizontal", 60 + TargetPage * LabelWidth, (100 + Mathf.Abs(LabelContainer.ScrollHorizontal - 60 - TargetPage * LabelWidth)) * 0.0008).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Sine);
         tween.TweenProperty(SwipeContainer, "scroll_horizontal", TargetPage * PageWidth, 0.6f).SetEase(Tween.EaseType.InOut).SetTrans(Tween.TransitionType.Cubic);
 >>>>>>> main
+=======
+        tween.TweenProperty(LabelContainer, "scroll_horizontal", 60 + TargetPage * LabelWidth, (100 + Mathf.Abs(LabelContainer.ScrollHorizontal - 60 - TargetPage * LabelWidth)) * 0.0008).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Sine);
+        tween.TweenProperty(SwipeContainer, "scroll_horizontal", TargetPage * PageWidth, 0.6f).SetEase(Tween.EaseType.InOut).SetTrans(Tween.TransitionType.Cubic);
+=======
+        tween.TweenCallback(Callable.From(() =>  LabelContainer.MouseFilter = MouseFilterEnum.Ignore));
+        tween.TweenCallback(Callable.From(() =>  MouseFilter = MouseFilterEnum.Ignore));
+
+        double Time = (100 + Mathf.Abs(LabelContainer.ScrollHorizontal - 60 - TargetPage * LabelWidth)) * 0.0008;
+
+        tween.TweenProperty(LabelContainer, "scroll_horizontal", 60 + TargetPage * LabelWidth, Time).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Sine);
+        tween.TweenProperty(SwipeContainer, "scroll_horizontal", TargetPage * PageWidth, 0.6f).SetEase(Tween.EaseType.InOut).SetTrans(Tween.TransitionType.Cubic);
+
+        tween.TweenCallback(Callable.From(() => LabelContainer.MouseFilter = MouseFilterEnum.Pass)).SetDelay(Time);
+        tween.TweenCallback(Callable.From(() => MouseFilter = MouseFilterEnum.Pass)).SetDelay(Time);
+>>>>>>> dev
+>>>>>>> Stashed changes
     }
     private void OnExitPressed() => GetParent<Hud>().AnimatePages(this, Hud.Pages[0]);
 }
